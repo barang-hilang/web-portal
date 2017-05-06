bfc1a0842fbf3433afd9215cea5e07ce9a5a41ee
igprad <ign.aldi.pradana@gmail.com>
2017-05-06 21:43:24 +0700

    Added post register at Register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,20 +1,82 @@
 var React = require('react');
+var axios = require('axios');
 
-var Register = React.createClass({
-  render: function(){
+class Register  extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {email: '',password:''};
+
+    this.loginCek = this.loginCek.bind(this);
+    this.handleChangeEmail = this.handleChangeEmail.bind(this);
+    this.handleChangePassword = this.handleChangePassword.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChangeEmail(event) {
+    this.setState({email: event.target.value});
+  }
+
+  handleChangePassword(event) {
+    this.setState({password: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.password);
+    event.preventDefault();
+  }
+
+  loginCek(event){
+    //agar bisa akses beda port, event normal/default dibatalkan
+    event.preventDefault();
+
+    axios.post('http://localhost:8080/api/v1/developers/',{
+      headers:{
+        'Content-Type' : 'application/json'
+      },
+      email : this.state.email,
+      password : this.state.password
+    })
+    .then((res)=>{
+      console.log(res.data);
+
+        if(res.data.message==="Developer Has Been Created"){
+          alert("Success Daftar");
+        }
+        else {
+          alert("Failed");
+
+        }
+      })
+    .catch(function(e) {
+      console.log(e.message);
+      console.log(e.code);
+      console.log(e.config);
+      console.log(e.response);
+        alert("Error");
+    });
+
+  }
+
+  render(){
     return(
       <div className="container RegisterPage">
         <h2>Register for free</h2>
         <hr/>
         <p>If you're developer, you can sign-up here</p>
-        <form>
+        <form onSubmit={this.loginCek.bind(this)}>
           <div className="form-group">
             <label for="usr">Username/Email :</label>
-            <input type="text" className="form-control" id="usr"/>
+            <input type="text" className="form-control" id="usr"
+              value={this.state.email}
+              onChange={this.handleChangeEmail}
+            />
           </div>
           <div className="form-group">
             <label for="pwd">Password:</label>
-            <input type="password" className="form-control" id="pwd"/>
+            <input type="password" className="form-control" id="pwd"
+              value={this.state.password}
+              onChange={this.handleChangePassword}
+            />
           </div>
           <div className="form-group">
             <label for="plan">Select Plan:</label>
@@ -37,6 +99,6 @@ var Register = React.createClass({
       </div>
     );
   }
-});
+};
 
 module.exports = Register;
